b9f70617917e430546c14d86ecc8b0dd2ed8f857
samarthunflagged <samarthnimeshkumarpatel@gmail.com>
2026-07-21 21:45:18 +0530

    Fix Leaflet marker icon paths

--- client/src/api/leaflet.js
+++ client/src/api/leaflet.js
@@ -1,6 +1,16 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 export const isMapCreated = mapId => {
   const mapEl = document.getElementById(mapId);
 
